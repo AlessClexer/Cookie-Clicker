e1c36a4f735416a2869307206f89e21f1b423222
artur <artur@DESKTOP-PLUM5J0>
2023-03-25 14:20:50 -0400

    Added new mechanics and cool stuff!

--- FunGame/Form1.Designer.vb
+++ FunGame/Form1.Designer.vb
@@ -25,7 +25,6 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Cookie_Value = New System.Windows.Forms.Label()
-        Me.btn_cookie_click = New System.Windows.Forms.Button()
         Me.PassiveCookieTimer = New System.Windows.Forms.Timer(Me.components)
         Me.Price_Text_1 = New System.Windows.Forms.Label()
         Me.Level_Text_1 = New System.Windows.Forms.Label()
@@ -33,34 +32,26 @@ Partial Class Form1
         Me.Price_Text_2 = New System.Windows.Forms.Label()
         Me.Button_Command_2 = New System.Windows.Forms.Button()
         Me.Level_Text_2 = New System.Windows.Forms.Label()
-        Me.btn_cookie_click_2 = New System.Windows.Forms.Button()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.RoundButton1 = New FunGame.RoundButton()
         Me.Cookie_Multiplier = New System.Windows.Forms.Label()
         Me.Cookie_Stores = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Achievement = New System.Windows.Forms.Label()
+        Me.cookie_workers = New System.Windows.Forms.Label()
+        Me.Price_Text_3 = New System.Windows.Forms.Label()
+        Me.Button_Command_3 = New System.Windows.Forms.Button()
+        Me.Remind_1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Cookie_Value
         '
         Me.Cookie_Value.AutoSize = True
-        Me.Cookie_Value.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Cookie_Value.Location = New System.Drawing.Point(302, 3)
+        Me.Cookie_Value.Font = New System.Drawing.Font("Comic Sans MS", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Cookie_Value.Location = New System.Drawing.Point(302, 8)
         Me.Cookie_Value.Name = "Cookie_Value"
-        Me.Cookie_Value.Size = New System.Drawing.Size(279, 81)
+        Me.Cookie_Value.Size = New System.Drawing.Size(241, 71)
         Me.Cookie_Value.TabIndex = 0
         Me.Cookie_Value.Text = "Cookie: 0"
-        '
-        'btn_cookie_click
-        '
-        Me.btn_cookie_click.Location = New System.Drawing.Point(651, 377)
-        Me.btn_cookie_click.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.btn_cookie_click.Name = "btn_cookie_click"
-        Me.btn_cookie_click.Size = New System.Drawing.Size(231, 64)
-        Me.btn_cookie_click.TabIndex = 1
-        Me.btn_cookie_click.Text = "Click To Increase Cookie By 1"
-        Me.btn_cookie_click.UseVisualStyleBackColor = True
         '
         'PassiveCookieTimer
         '
@@ -69,29 +60,29 @@ Partial Class Form1
         '
         Me.Price_Text_1.AutoSize = True
         Me.Price_Text_1.BackColor = System.Drawing.Color.Goldenrod
-        Me.Price_Text_1.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Price_Text_1.Location = New System.Drawing.Point(47, 38)
+        Me.Price_Text_1.Font = New System.Drawing.Font("Comic Sans MS", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Price_Text_1.Location = New System.Drawing.Point(45, 25)
         Me.Price_Text_1.Name = "Price_Text_1"
-        Me.Price_Text_1.Size = New System.Drawing.Size(115, 45)
+        Me.Price_Text_1.Size = New System.Drawing.Size(154, 56)
         Me.Price_Text_1.TabIndex = 2
         Me.Price_Text_1.Text = "Price 2"
         '
         'Level_Text_1
         '
         Me.Level_Text_1.AutoSize = True
-        Me.Level_Text_1.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level_Text_1.Location = New System.Drawing.Point(64, 150)
+        Me.Level_Text_1.Font = New System.Drawing.Font("Comic Sans MS", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Level_Text_1.Location = New System.Drawing.Point(77, 149)
         Me.Level_Text_1.Name = "Level_Text_1"
-        Me.Level_Text_1.Size = New System.Drawing.Size(82, 45)
+        Me.Level_Text_1.Size = New System.Drawing.Size(87, 45)
         Me.Level_Text_1.TabIndex = 3
-        Me.Level_Text_1.Text = "Lvl 0"
+        Me.Level_Text_1.Text = "Lvl 1"
         '
         'Button_Command_1
         '
-        Me.Button_Command_1.Location = New System.Drawing.Point(13, 87)
+        Me.Button_Command_1.Location = New System.Drawing.Point(12, 80)
         Me.Button_Command_1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button_Command_1.Name = "Button_Command_1"
-        Me.Button_Command_1.Size = New System.Drawing.Size(173, 61)
+        Me.Button_Command_1.Size = New System.Drawing.Size(213, 65)
         Me.Button_Command_1.TabIndex = 4
         Me.Button_Command_1.Text = "Increase Value of Click"
         Me.Button_Command_1.UseVisualStyleBackColor = True
@@ -100,19 +91,19 @@ Partial Class Form1
         '
         Me.Price_Text_2.AutoSize = True
         Me.Price_Text_2.BackColor = System.Drawing.Color.Goldenrod
-        Me.Price_Text_2.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Price_Text_2.Location = New System.Drawing.Point(696, 39)
+        Me.Price_Text_2.Font = New System.Drawing.Font("Comic Sans MS", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Price_Text_2.Location = New System.Drawing.Point(653, 20)
         Me.Price_Text_2.Name = "Price_Text_2"
-        Me.Price_Text_2.Size = New System.Drawing.Size(149, 45)
+        Me.Price_Text_2.Size = New System.Drawing.Size(196, 56)
         Me.Price_Text_2.TabIndex = 8
         Me.Price_Text_2.Text = "Price 100"
         '
         'Button_Command_2
         '
-        Me.Button_Command_2.Location = New System.Drawing.Point(682, 87)
+        Me.Button_Command_2.Location = New System.Drawing.Point(635, 80)
         Me.Button_Command_2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button_Command_2.Name = "Button_Command_2"
-        Me.Button_Command_2.Size = New System.Drawing.Size(185, 60)
+        Me.Button_Command_2.Size = New System.Drawing.Size(233, 70)
         Me.Button_Command_2.TabIndex = 9
         Me.Button_Command_2.Text = "Click to Buy Cookie Stores"
         Me.Button_Command_2.UseVisualStyleBackColor = True
@@ -120,31 +111,12 @@ Partial Class Form1
         'Level_Text_2
         '
         Me.Level_Text_2.AutoSize = True
-        Me.Level_Text_2.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level_Text_2.Location = New System.Drawing.Point(738, 151)
+        Me.Level_Text_2.Font = New System.Drawing.Font("Comic Sans MS", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Level_Text_2.Location = New System.Drawing.Point(704, 154)
         Me.Level_Text_2.Name = "Level_Text_2"
-        Me.Level_Text_2.Size = New System.Drawing.Size(82, 45)
+        Me.Level_Text_2.Size = New System.Drawing.Size(87, 45)
         Me.Level_Text_2.TabIndex = 10
-        Me.Level_Text_2.Text = "Lvl 0"
-        '
-        'btn_cookie_click_2
-        '
-        Me.btn_cookie_click_2.Location = New System.Drawing.Point(651, 305)
-        Me.btn_cookie_click_2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.btn_cookie_click_2.Name = "btn_cookie_click_2"
-        Me.btn_cookie_click_2.Size = New System.Drawing.Size(234, 64)
-        Me.btn_cookie_click_2.TabIndex = 11
-        Me.btn_cookie_click_2.Text = "Click To Increase Cookie By 1"
-        Me.btn_cookie_click_2.UseVisualStyleBackColor = True
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 282)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(243, 230)
-        Me.PictureBox1.TabIndex = 12
-        Me.PictureBox1.TabStop = False
+        Me.Level_Text_2.Text = "Lvl 1"
         '
         'RoundButton1
         '
@@ -172,7 +144,7 @@ Partial Class Form1
         '
         Me.Cookie_Stores.AutoSize = True
         Me.Cookie_Stores.Font = New System.Drawing.Font("Comic Sans MS", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Cookie_Stores.Location = New System.Drawing.Point(273, 396)
+        Me.Cookie_Stores.Location = New System.Drawing.Point(273, 438)
         Me.Cookie_Stores.Name = "Cookie_Stores"
         Me.Cookie_Stores.Size = New System.Drawing.Size(283, 45)
         Me.Cookie_Stores.TabIndex = 15
@@ -184,43 +156,90 @@ Partial Class Form1
         Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label1.Location = New System.Drawing.Point(662, 246)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(206, 55)
+        Me.Label1.Size = New System.Drawing.Size(0, 55)
         Me.Label1.TabIndex = 16
-        Me.Label1.Text = "IGNORE:"
+        '
+        'Achievement
+        '
+        Me.Achievement.AutoSize = True
+        Me.Achievement.Font = New System.Drawing.Font("Comic Sans MS", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Achievement.Location = New System.Drawing.Point(12, 540)
+        Me.Achievement.Name = "Achievement"
+        Me.Achievement.Size = New System.Drawing.Size(1000, 45)
+        Me.Achievement.TabIndex = 17
+        Me.Achievement.Text = "                                                                      "
+        '
+        'cookie_workers
+        '
+        Me.cookie_workers.AutoSize = True
+        Me.cookie_workers.Font = New System.Drawing.Font("Comic Sans MS", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.cookie_workers.Location = New System.Drawing.Point(273, 393)
+        Me.cookie_workers.Name = "cookie_workers"
+        Me.cookie_workers.Size = New System.Drawing.Size(311, 45)
+        Me.cookie_workers.TabIndex = 18
+        Me.cookie_workers.Text = "Cookie Workers: 0"
+        '
+        'Price_Text_3
+        '
+        Me.Price_Text_3.AutoSize = True
+        Me.Price_Text_3.Font = New System.Drawing.Font("Comic Sans MS", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Price_Text_3.Location = New System.Drawing.Point(662, 218)
+        Me.Price_Text_3.Name = "Price_Text_3"
+        Me.Price_Text_3.Size = New System.Drawing.Size(178, 56)
+        Me.Price_Text_3.TabIndex = 19
+        Me.Price_Text_3.Text = "Price 50"
+        '
+        'Button_Command_3
+        '
+        Me.Button_Command_3.Location = New System.Drawing.Point(635, 276)
+        Me.Button_Command_3.Name = "Button_Command_3"
+        Me.Button_Command_3.Size = New System.Drawing.Size(246, 69)
+        Me.Button_Command_3.TabIndex = 20
+        Me.Button_Command_3.Text = "Click to Buy Workers"
+        Me.Button_Command_3.UseVisualStyleBackColor = True
+        '
+        'Remind_1
+        '
+        Me.Remind_1.AutoSize = True
+        Me.Remind_1.Font = New System.Drawing.Font("Comic Sans MS", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Remind_1.Location = New System.Drawing.Point(2, 483)
+        Me.Remind_1.Name = "Remind_1"
+        Me.Remind_1.Size = New System.Drawing.Size(0, 45)
+        Me.Remind_1.TabIndex = 21
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Goldenrod
-        Me.ClientSize = New System.Drawing.Size(911, 506)
+        Me.ClientSize = New System.Drawing.Size(910, 594)
+        Me.Controls.Add(Me.Remind_1)
+        Me.Controls.Add(Me.Button_Command_3)
+        Me.Controls.Add(Me.Price_Text_3)
+        Me.Controls.Add(Me.cookie_workers)
+        Me.Controls.Add(Me.Achievement)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Cookie_Stores)
         Me.Controls.Add(Me.Cookie_Multiplier)
         Me.Controls.Add(Me.RoundButton1)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.btn_cookie_click_2)
         Me.Controls.Add(Me.Level_Text_2)
         Me.Controls.Add(Me.Button_Command_2)
         Me.Controls.Add(Me.Price_Text_2)
         Me.Controls.Add(Me.Button_Command_1)
         Me.Controls.Add(Me.Level_Text_1)
         Me.Controls.Add(Me.Price_Text_1)
-        Me.Controls.Add(Me.btn_cookie_click)
         Me.Controls.Add(Me.Cookie_Value)
         Me.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Form1"
         Me.Text = "Cookie Clicker"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Cookie_Click As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents Achievement As Label
     Friend WithEvents Cookie_Value As Label
-    Friend WithEvents btn_cookie_click As Button
     Friend WithEvents PassiveCookieTimer As Timer
     Friend WithEvents Price_Text_1 As Label
     Friend WithEvents Level_Text_1 As Label
@@ -228,10 +247,12 @@ Partial Class Form1
     Friend WithEvents Price_Text_2 As Label
     Friend WithEvents Button_Command_2 As Button
     Friend WithEvents Level_Text_2 As Label
-    Friend WithEvents btn_cookie_click_2 As Button
-    Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents RoundButton1 As RoundButton
     Friend WithEvents Cookie_Multiplier As Label
     Friend WithEvents Cookie_Stores As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents cookie_workers As Label
+    Friend WithEvents Price_Text_3 As Label
+    Friend WithEvents Button_Command_3 As Button
+    Friend WithEvents Remind_1 As Label
 End Class
--- FunGame/Form1.vb
+++ FunGame/Form1.vb
@@ -2,30 +2,30 @@
 Imports System.Net
 Imports System.Security.Cryptography
 Imports System.Security.Principal
+Imports System.Threading
 
 Public Class Form1
-    Public totalCookies, ClickValue, ClickLevel, ClickLevel2 As Integer
+    Public totalCookies, ClickValue, ClickLevel, CookieStores, CookieWorkers As Integer
     Public PassiveOneVal, PassiveOneLevel As Integer
+    Public WorkerValue As Integer
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         totalCookies = 0
-        ClickValue = 1
+        ClickValue = 0
         ClickLevel = 1
         PassiveOneVal = 0
         PassiveOneLevel = 1
+        WorkerValue = 0
+
+        CookieStores = 0
+        CookieWorkers = 0
         set_Timer(100)
 
         PassiveCookieTimer.Start()
     End Sub
 
-    Private Sub btn_cookie_click_Click(sender As Object, e As EventArgs) Handles btn_cookie_click.Click
+    Private Sub btn_cookie_click_Click(sender As Object, e As EventArgs)
         click_cookie()
     End Sub
-
-    'new github test'
-
-    'Private Sub btn_cookie_click_2_Click(sender As Object, e As EventArgs) Handles btn_cookie_click_2.Click
-    'click_cookie()
-    'End Sub
 
     Private Sub click_cookie()
         totalCookies = totalCookies + ClickValue
@@ -34,6 +34,17 @@ Public Class Form1
 
     Private Sub RoundButton1_Click(sender As Object, e As EventArgs) Handles RoundButton1.Click
         click_cookie()
+    End Sub
+
+    Public Sub Button_Command_3_Click(sender As Object, e As EventArgs) Handles Button_Command_3.Click
+        If totalCookies >= 50 Then
+            totalCookies -= 50
+
+            WorkerValue += 1
+
+            cookie_workers.Text = "Cookie Workers: " + WorkerValue.ToString
+
+        End If
     End Sub
 
     Private Sub Button_Command_1_Click(sender As Object, e As EventArgs) Handles Button_Command_1.Click
@@ -46,7 +57,7 @@ Public Class Form1
             ClickLevel = ClickLevel + 1
             ClickValue = (ClickLevel - 1) * 5
 
-            upgradeCost = ClickLevel * 2
+            upgradeCost = ClickLevel * 1
             Level_Text_1.Text = "Lvl: " + ClickLevel.ToString
             Price_Text_1.Text = "Price: " + upgradeCost.ToString
             Cookie_Multiplier.Text = "Cookie Multiplier: " + ClickValue.ToString
@@ -56,17 +67,31 @@ Public Class Form1
     Private Sub Button_Command_2_Click(sender As Object, e As EventArgs) Handles Button_Command_2.Click
         Dim upgradeCost As Integer
         upgradeCost = PassiveOneLevel * PassiveOneLevel * 100
-        If totalCookies >= upgradeCost Then
+
+        If totalCookies >= upgradeCost And WorkerValue >= 10 Then
+            Remind_1.Text = " "
             totalCookies = totalCookies - upgradeCost
+            WorkerValue = WorkerValue - 10
 
             PassiveOneLevel = PassiveOneLevel + 1
             PassiveOneVal = PassiveOneVal + 1
+            CookieStores = PassiveOneVal
             upgradeCost = PassiveOneLevel * PassiveOneLevel * 100
 
             Level_Text_2.Text = "Lvl " + PassiveOneLevel.ToString
             Price_Text_2.Text = "Price: " + upgradeCost.ToString
             Cookie_Stores.Text = "Cookie Stores: " + PassiveOneVal.ToString
+        Else
+            Remind_1.Text = "You do not have enough money or workers to buy stores."
         End If
+
+        If CookieStores = 5 Then
+            Achievement.Text = "Congratulations! You have reached " + CookieStores.ToString + " Cookie Stores!"
+        End If
+        If CookieStores > 5 Then
+            Achievement.Text = " "
+        End If
+
     End Sub
     Private Sub PassiveCookieTimer_Tick(sender As Object, e As EventArgs) Handles PassiveCookieTimer.Tick
         totalCookies = totalCookies + 10
@@ -76,6 +101,5 @@ Public Class Form1
     Private Sub set_Timer(interval As Integer)
         PassiveCookieTimer.Interval = interval
     End Sub
-
 
 End Class
